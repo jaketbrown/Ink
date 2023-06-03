9c0f23b3f240a2a5cc424833700df9bab5d989e0
Jake Brown <44589911+jaketbrown@users.noreply.github.com>
2023-06-02 23:43:46 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,11 +12,22 @@ let package = Package(
     name: "Ink",
     products: [
         .library(name: "Ink", targets: ["Ink"]),
-        .executable(name: "ink-cli", targets: ["InkCLI"])
+        .executable(name: "ink-cli", targets: ["InkCLI"]),
+        .executable(name: "fuzz-ink", targets: ["InkFuzz"])
     ],
     targets: [
         .target(name: "Ink"),
         .target(name: "InkCLI", dependencies: ["Ink"]),
+        .target(name: "InkFuzz", dependencies: ["Ink"],
+                path: "mayhem",
+                sources: ["main.swift", "FuzzedDataProvider.swift"],
+                swiftSettings: [
+                    .unsafeFlags(["-sanitize=fuzzer,address"]),
+                    .unsafeFlags(["-parse-as-library"])],
+                linkerSettings: [
+                    .unsafeFlags(["-sanitize=fuzzer,address"])
+                ]
+        ),
         .testTarget(name: "InkTests", dependencies: ["Ink"])
     ]
 )
